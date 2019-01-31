03c9fa446994345e68ec6142c88515bdcacabefe
saul-lzd <saulopez@SAULOPEZ-LAP.oradev.oraclecorp.com>
2019-01-30 18:12:56 -0600

    # Unable to create Derby database.

--- src/main/java/dao/Derby.java
+++ src/main/java/dao/Derby.java
@@ -26,7 +26,7 @@ public class Derby {
 
     protected static Connection connection;
     //private static String dbUrl = "jdbc:derby:." + File.separator + "database;create=true";
-    private static String dbUrl = "jdbc:derby:database" + File.separator + "db;create=true";
+    private static String dbUrl = "jdbc:derby:database" + File.separator + "db";
 //    private static String dbUrl = "jdbc:derby:;databaseName=localDB";
 
     public static void init() throws SQLException {
